494c06511bfeb969a75156aa0cbde155c8456bef
huangyongbo <13453219820@163.com>
2022-01-19 20:23:08 +0800

    demo3:while

--- src/demo2/com/Demo3.java
+++ src/demo2/com/Demo3.java
@@ -3,14 +3,19 @@ package demo2.com;
 public class Demo3 {
 
     public static void main(String[] args) {
-
+        test1();
     }
 
     /**
      *
      */
     public static void test1(){
+        int count = 0;
 
+        while(count < 100){
+            System.out.println("hello woid" + count);
+            count++;
+        }
     }
 
 }
